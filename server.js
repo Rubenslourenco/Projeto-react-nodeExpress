e3ea39c2771f9d6c6aa86edc798b1a4b2eb1a033
Rubens Alves <alvesrubens926@gmail.com>
2024-10-08 23:36:26 -0300

    Request query params, route , body, mongo

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
-
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
-    res.send('OK aqui deu certo')
+app.post('/usuarios', async (req, res) => { 
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
     
+    res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req , res) => {
-    res.json(users)
+app.get('/usuarios', async(req , res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.put('/usuarios/:id', async (req, res) => { 
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    
+    res.status(201).json(req.body)
 })
 
 app.listen(3000)
