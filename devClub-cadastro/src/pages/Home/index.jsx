070007a3bdbc05a85c8ce4b7ae8218e33cb17af8
Rubens Alves <alvesrubens926@gmail.com>
2024-10-09 23:18:29 -0300

    Criando cadastro html

---
+++ devClub-cadastro/src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+function Home() {
+  return (
+    <div>
+      <form action="">
+        <h1>Cadastro de Usuarios</h1>
+        <input type="text" name="nome" id="" />
+        <input type="number" name="idade" id="" />
+        <input type="email" name="email" id="" />
+        <button type="button"></button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
